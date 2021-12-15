f2015b8a641f5422fcd05536a5e346252e5aee90
svarkhinos <svitlana.arkhinos@gmail.com>
2021-12-15 19:55:42 +0200

    fin

---
+++ README.md
@@ -0,0 +1,4 @@
+list https://prnt.sc/2354ewf
+find by id https://prnt.sc/2354nsj
+add contact https://prnt.sc/2355ysj
+delete contact https://prnt.sc/2356n27
---
+++ contacts.js
@@ -0,0 +1,35 @@
+const fs = require("fs/promises");
+const path = require("path");
+const crypto = require("crypto");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const listContacts = async () => {
+  const content = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(content);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+  const [result] = contacts.filter((contact) => contact.id === contactId);
+  return result;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+  const newContacts = contacts.filter((contact) => contact.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+  return newContacts;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+  const newContact = { name, email, phone, id: crypto.randomUUID() };
+
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
